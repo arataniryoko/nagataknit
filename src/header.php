92bdf1c5abc365c78a137301693791e648661cad
arataniryoko <r.aratani@furazoa.com>
2021-08-02 17:04:39 +0900

    no message

--- src/header.php
+++ src/header.php
@@ -3,18 +3,6 @@
 <html <?php language_attributes(); ?>>
 
 <head>
-  <!-- Global site tag (gtag.js) - Google Analytics
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-000000000"></script>
--->
-  <script>
-  /*
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-	gtag('config', 'UA-00000000');
-	*/
-  </script>
   <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>">
   <title><?php wp_title(); ?></title>
   <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1.0, user-scalable=yes">
